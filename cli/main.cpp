09f367f343ad927c708201b57ea56200b6fd63ca
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2021-03-13 13:17:11 -0800

    make linking automatic

--- cli/main.cpp
+++ cli/main.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
+#include <cstdlib>
 #include <fstream>
+#include <iostream>
 #include <sstream>
 
 #include <tcc/tcc.hpp>
@@ -21,7 +22,7 @@ int main(int argc, char **argv) {
 
   tcc::ast::Program program;
   try {
-program =  tcc::parser::parseTokens(tokens);
+    program = tcc::parser::parseTokens(tokens);
   } catch (const tcc::parser::Exception &e) {
     std::cerr << "Parsing error:\n";
     std::cerr << '\t' << e.what() << '\n';
@@ -30,6 +31,20 @@ program =  tcc::parser::parseTokens(tokens);
 
   std::ofstream outputFile(outputPath);
   tcc::codegen::codegen(outputFile, program);
+  outputFile.close();
+
+#ifdef _WIN32
+  std::string linker = "clang.exe";
+  std::string postfix = ".exe";
+#else
+  std::string linker = "gcc";
+  std::string postfix = "";
+#endif // _WIN32
+
+  std::string command = linker + " \"" + outputPath + "\"" + " -o \"" +
+                        inputBase + postfix + "\"";
+
+  std::system(command.c_str());
 
   return 0;
 }
